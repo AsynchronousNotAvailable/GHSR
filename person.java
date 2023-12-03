985d24017adcbb48e5cbc73681e1f40fc09452dc
Weng Hong :D <whleexd@gmail.com>
2023-12-03 13:29:01 +0800

    new branch commit

--- person.java
+++ person.java
@@ -2,8 +2,8 @@ public class person {
     public static void main(String[] args) {
 
         System.out.print(addNum(1, 2));
-        System.out.println("This is a commit from main branch");
-        System.out.println("Hello world");
+        System.out.println("This is a commit from new branch");
+        System.out.println("Hello branch");
     }
     
     static int addNum(int a, int b) {
