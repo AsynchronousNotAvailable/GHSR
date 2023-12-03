1414f9835a22ed08aeea4ac664501263bc7dc6ec
Weng Hong :D <whleexd@gmail.com>
2023-12-03 13:18:19 +0800

    commit main

--- person.java
+++ person.java
@@ -2,6 +2,7 @@ public class person {
     public static void main(String[] args) {
         
         System.out.print(addNum(1, 2));
+        System.out.println("This is a commit from main branch");
     }
     
     static int addNum(int a, int b) {
