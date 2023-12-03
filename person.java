a2a0dbe0871dbabf71e5e5a1d6f27176a1a1382d
Weng Hong :D <whleexd@gmail.com>
2023-12-03 11:23:10 +0800

    print

--- person.java
+++ person.java
@@ -1,5 +1,6 @@
 public class person {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
+        System.out.print(sayHello());
         System.out.println("This is a test");
     }
     
