cd56328751821bad8cac7723484b5902685c3d53
Lee Weng Hong <whleexd@gmail.com>
2023-12-03 11:12:47 +0800

    Update person.java

--- person.java
+++ person.java
@@ -1,5 +1,12 @@
 public class person {
-    public static void main(String [] args) {
-        System.out.println("This is a test");
+   public static void main(String[] args) {
+        System.out.print(sayHello());
     }
+    
+    static String sayHello() {
+        return new String("Hello World");
+    }
+
+
+    
 }
