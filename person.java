909de38aaea61ed746f6d2cdce5ec0862f8a9656
Weng Hong :D <whleexd@gmail.com>
2023-12-03 13:33:45 +0800

    main commit

--- person.java
+++ person.java
@@ -1,13 +1,9 @@
 public class person {
     public static void main(String[] args) {
-
-        System.out.print(addNum(1, 2));
-        System.out.println("This is a commit from new branch");
-        System.out.println("Hello branch");
+        System.out.print(sayHi());
     }
     
-    static int addNum(int a, int b) {
-        System.out.println("Hello world");
-        return a + b + 2 + 90 * b;
+    static String sayHi() {
+        return new String("This is a main branch commit");
     }
 }
