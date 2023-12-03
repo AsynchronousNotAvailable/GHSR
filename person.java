7e84953aa366d1bfd369b520b16485d8b9329b3c
Lee Weng Hong <whleexd@gmail.com>
2023-12-04 00:34:48 +0800

    Update person.java

--- person.java
+++ person.java
@@ -1,6 +1,6 @@
 public class person {
     public static void main(String[] args) {
-        System.out.print("Hello World from new branch");
+        System.out.print("Hello World");
         System.out.println("This is the new branch");
     }
     
