e6722aafae777f995beab1f27b71e1ba4135046d
Weng Hong :D <whleexd@gmail.com>
2023-12-03 11:32:00 +0800

    back to ori

--- person.java
+++ person.java
@@ -1,10 +1,6 @@
 public class person {
     public static void main(String[] args) {
-        System.out.print(sayHello());
         System.out.println("This is a test");
     }
     
-    static String sayHello(){
-        return new String("Hello World");
-    }
 }
