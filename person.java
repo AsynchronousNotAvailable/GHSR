008b565f67004461eefc75a72751e4f8f3dc361d
Lee Weng Hong <whleexd@gmail.com>
2023-12-03 12:43:50 +0800

    Update person.java

--- person.java
+++ person.java
@@ -1,6 +1,5 @@
 public class person {
     public static void main(String[] args) {
-        System.out.println("Lorem ipsum ksdjkadj");
         System.out.println("qwihefruiwhf");
         System.out.print(addNum(1, 2));
     }
