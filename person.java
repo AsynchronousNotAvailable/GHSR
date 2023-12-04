c34c6e9fb7ec389ff1c086ed83bd2ced83849fcb
AsynchronousNotAvailable <whleexd@gmail.com>
2023-12-04 08:14:46 +0800

    init

--- person.java
+++
@@ -1,7 +0,0 @@
-public class person {
-    public static void main(String[] args) {
-        System.out.print("Hello World from new branch");
-        System.out.println("This is the new branch");
-    }
-    
-}
